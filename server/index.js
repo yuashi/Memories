77e68b74de5e9e507e6848423078883aba80c909
yuashi <singh9ayushi9@gmail.com>
2022-01-26 20:17:37 +0530

    deployed with basic functionalities

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello! Welcome to the Memories app. ");
+});
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 
